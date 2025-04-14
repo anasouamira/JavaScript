fadece5bc55e300c31509352297911129ea57dee
Ouamira <anasouamira@gmail.com>
2025-04-14 15:05:31 +0100

    Created By Anas Ouamira

--- 10_String/script.js
+++ 10_String/script.js
@@ -7,14 +7,15 @@ Length: ${str.length} <br>
 First character: ${str[0] || "N/A"} <br>
 To Uppercase: ${str.toUpperCase()} <br>
 To Lowercase: ${str.toLowerCase()} <br>
-Includes 'lo': ${str.includes('lo')} <br>
+Includes 'Ou': ${str.includes('Ou')} <br>
 Index of 'o': ${str.indexOf('o')} <br>
 Slice (1, 4): ${str.slice(1, 4)} <br>
-Replace 'l' with 'x': ${str.replace('l', 'x')} <br>
+Replace 'i' with 'x': ${str.replace('i', 'x')} <br>
 Trimmed: "${str.trim()}" <br>
 Split by space: ${JSON.stringify(str.split(" "))} <br>
 Substring (1, 4): ${str.substring(1, 4)} <br>
 Substr (1, 4): ${str.substr(1, 4)} <br>
+Search for 'Anas': ${str.search('Anas')} <br>
 `;
 
     document.getElementById("stringResult").innerHTML = output;
